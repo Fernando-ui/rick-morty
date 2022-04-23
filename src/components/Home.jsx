586d611c7a2102fb1f37483625e1a02ddb5709b1
fernando-ui <fst-96@hotmail.com>
2022-04-22 21:00:58 -0500

    colocando flehcas de cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import HomeLayout from "../sass/layout/Inicio.module.scss";
 import { CardEpisodes } from "./episodes/CardEpisodes";
-
+import "../sass/components/arrowButton.scss";
 
 export const Home = () => {
   return (
@@ -14,8 +14,35 @@ export const Home = () => {
               Episodes
               <hr />
             </div>
-            <div className='container__arrow'></div>
-              <CardEpisodes />
+            <div className="container__arrow container__arrow-left">
+              <svg
+                class="container__button-left"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-t="angle-left-svg"
+                aria-labelledby="angle-svg"
+                aria-hidden="true"
+                role="img"
+              >
+                <title id="angle-svg">Anterior</title>
+                <path d="M8.6 7.4L10 6l6 6-6 6-1.4-1.4 4.6-4.6z"></path>
+              </svg>
+            </div>
+            <CardEpisodes />
+            <div className="container__arrow container__arrow-right">
+              <svg
+                class="container__button-right"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-t="angle-left-svg"
+                aria-labelledby="angle-svg"
+                aria-hidden="true"
+                role="img"
+              >
+                <title id="angle-svg">Anterior</title>
+                <path d="M8.6 7.4L10 6l6 6-6 6-1.4-1.4 4.6-4.6z"></path>
+              </svg>
+            </div>
           </div>
         </div>
         <div className={`${HomeLayout.container__locations}`}>
@@ -23,8 +50,7 @@ export const Home = () => {
             <div className={`${HomeLayout.container__flex__title}`}>
               Characteres
             </div>
-            <div>
-            </div>
+            <div></div>
           </div>
         </div>
         <div className={`${HomeLayout.container__locations}`}>
@@ -32,8 +58,7 @@ export const Home = () => {
             <div className={`${HomeLayout.container__flex__title}`}>
               Locations
             </div>
-            <div>
-            </div>
+            <div></div>
           </div>
         </div>
       </div>
